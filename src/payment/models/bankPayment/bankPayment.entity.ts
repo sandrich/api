293f3e2777c933849c0749c332f01bed39410115
Yannick1712 <nickbusch1998@gmail.com>
2021-10-11 01:13:48 +0200

    add some standard columns

--- src/payment/models/bankPayment/bankPayment.entity.ts
+++ src/payment/models/bankPayment/bankPayment.entity.ts
@@ -1,4 +1,13 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany, UpdateDateColumn } from 'typeorm';
+import { Fiat } from 'src/shared/models/fiat/fiat.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
 
 export enum BankPaymentType {
   DFXPAYMENT = 'DFX-Payment',
@@ -16,6 +25,33 @@ export class BankPayment {
 
   @Column({ type: 'varchar', length: 256, unique: true })
   type: BankPaymentType;
+
+  @ManyToOne(() => Fiat, { eager: true })
+  fiat: Fiat;
+
+  @Column({ type: 'float', nullable: true })
+  fiatValue: number;
+
+  @Column({ length: 256, unique: true })
+  bankTransactionId: string;
+
+  @Column({ length: 256, nullable: true })
+  iban: string;
+
+  @Column({ length: 256 })
+  bankUsage: string;
+
+  @Column({ length: 256 })
+  name: string;
+
+  @Column({ length: 256 })
+  location: string;
+
+  @Column()
+  bookingDate: Date;
+
+  @Column()
+  validationDate: Date;
 
   //TODO Spalten aus xml eintragen
 
